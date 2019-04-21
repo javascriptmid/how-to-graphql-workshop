9ddd60dce052d28ee97a9c3c80a395b6bdc2927d
Jerome Olvera <jerome.olvera@gmail.com>
2019-04-20 20:25:29 -0500

    Local Prisma Server example

--- packages/server/src/generated/prisma-client/index.js
+++ packages/server/src/generated/prisma-client/index.js
@@ -20,6 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://us1.prisma.sh/jerome-olvera/server/dev`
+  endpoint: `http://localhost:4466/`
 });
 exports.prisma = new exports.Prisma();
